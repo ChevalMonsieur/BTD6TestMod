9a5c774a020c4c9ebd68b7cc0c1555e50080dc0e
ChevalMonsieur <just.1et2mii@gmail.com>
2024-10-23 15:22:13 +0200

    update mod to 0.0.1

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,11 +3,11 @@ namespace testMod;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "44.0";
-    public const string Version = "1.0.0";
+    public const string Version = "0.0.1";
     public const string Name = "testMod";
 
-    public const string Description = "An empty mod";
+    public const string Description = "Testing";
 
     public const string RepoOwner = "ChevalMonsieur"; // TODO add your github username hero, also in the download url in README.md
-    public const string RepoName = ""; // TODO add your repo name here, also in the download url in README.md
+    public const string RepoName = "BTD6TestMod"; // TODO add your repo name here, also in the download url in README.md
 }
--- testMod.cs
+++ testMod.cs
@@ -1,6 +1,9 @@
 using MelonLoader;
 using BTD_Mod_Helper;
 using testMod;
+using Il2CppAssets.Scripts.Data.Behaviors.Simulations;
+using Il2CppAssets.Scripts.Unity.Map;
+using System.Diagnostics;
 
 [assembly: MelonInfo(typeof(testMod.testMod), ModHelperData.Name, ModHelperData.Version, ModHelperData.RepoOwner)]
 [assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
@@ -12,5 +15,11 @@ public class testMod : BloonsTD6Mod
     public override void OnApplicationStart()
     {
         ModHelper.Msg<testMod>("testMod loaded!");
+    }
+
+    public override void OnRoundStart()
+    {
+        base.OnRoundStart();
+        Debug.WriteLine("Bruh round started");
     }
 }
